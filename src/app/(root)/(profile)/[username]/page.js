4595ad0fb17ec90216d56345b733b507b0c22b02
Ko-Kwan Mongkholtham <ceo.delamory@gmail.com>
2024-08-04 19:22:26 +0700

    feat: init clerk auth

---
+++ src/app/(root)/(profile)/[username]/page.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const ProfilePage = () => {
+  return (
+    <section>
+      <Text>ProfilePage</Text>
+    </section>
+  )
+}
+
+export default ProfilePage
